bac68834352399860e5c3fcbc2ecf76245d4e70a
mwsb <mwsilvabreen@gmail.com>
2014-12-11 20:35:14 -0600

    Added more metrics
    Added more scoring details.

--- wumpus/Test.java
+++ wumpus/Test.java
@@ -124,11 +124,19 @@ public class Test
 	{	
 		GameBoard[] boards = {new GameBoard(mediumBoard1),new GameBoard(mediumBoard2),new GameBoard(mediumBoard3),new GameBoard(mediumBoard4)};
 		double avgHHero = 0;
-		double avgPath = 0;
-		int dfsPath = 0;
-		int bfsPath = 0;
-		int dfsDeath = 0;
-		int bfsDeath = 0;
+		double hNodes = 0;
+		double hLength = 0;
+		
+		double dfsNodes = 0;
+		double dfsDeath = 0;
+		double dfsLength = 0;
+		
+		double bfsNodes = 0;
+		double bfsDeath = 0;
+		double bfsLength = 0;
+		
+		
+		
 		int loop = 0;
 		Random rand = new Random();
 		for(; loop < 1000; loop++)
@@ -160,33 +168,43 @@ public class Test
 				}
 				System.out.println("Nodes visited: " + hero.getNodesVisited());
 				System.out.println("Times died: " + died);
+				System.out.println("Solution path length: " + solutionPathLength);
+				System.out.println("-----------------------------------------------");
 				if(hero.getClass().getSimpleName().equals("HeuristicHero")){
 					avgHHero += died;
-					avgPath += hero.getNodesVisited();
+					hNodes += hero.getNodesVisited();
+					hLength += solutionPathLength;
 				}
 				else if (hero.getClass().getSimpleName().equals("BreadthSearchHero"))
 				{
-					bfsPath = hero.getNodesVisited();
+					bfsNodes += hero.getNodesVisited();
 					bfsDeath += died;
+					bfsLength += solutionPathLength;
 				}
 				else
 				{
-					dfsPath = hero.getNodesVisited();
+					dfsNodes += hero.getNodesVisited();
 					dfsDeath += died;
+					dfsLength += solutionPathLength;
 				}
-				System.out.println("Solution path length: " + solutionPathLength);
-				System.out.println("-----------------------------------------------");
 			}
 		}
-		System.out.println("Run " + loop + " times");
-		System.out.println("Average heuristic hero deaths: " + avgHHero/1000);
-		System.out.println("Average heuristic hero path length: " + avgPath/1000);
-		System.out.println("Heuristic final score: " + (10000.0 - (200.0*(avgHHero/1000)) - (avgPath/1000) * 3.0));
-		System.out.println("BFS average deaths: " + bfsDeath/1000);
-		System.out.println("BFS average path length: " + bfsPath/1000);
-		System.out.println("Heuristic final score: " + (10000.0 - (200.0*(avgHHero/1000)) - (avgPath/1000) * 3.0));
-		System.out.println("DFS average deaths: " + dfsDeath/1000);
-		System.out.println("DFS average path length: " + dfsPath/1000);
-		System.out.println("Heuristic final score: " + (10000.0 - (200.0*(avgHHero/1000)) - (avgPath/1000) * 3.0));
+		System.out.println("Ran " + loop + " times");
+		System.out.println("-----------------------------------------------");
+		System.out.println("Average heuristic hero deaths: " + avgHHero/loop);
+		System.out.println("Average heuristic nodes visited: " + hNodes/loop);
+		System.out.println("Average heuristic path length: " + hLength/loop);
+		System.out.println("Heuristic final score: " + (10000.0 - (200.0*(avgHHero/loop)) - ((hNodes/loop) * 3.0) - ((hLength/loop) * 4.0)));
+		System.out.println("-----------------------------------------------");
+		System.out.println("BFS average deaths: " + bfsDeath/loop);
+		System.out.println("BFS average nodes visited: " + bfsNodes/loop);
+		System.out.println("Average bfs path length: " + bfsLength/loop);
+		System.out.println("BFS final score: " + (10000.0 - (200.0*(bfsDeath/loop)) - ((bfsNodes/loop) * 3.0)- ((bfsLength/loop) * 4.0)));
+		System.out.println("-----------------------------------------------");
+		System.out.println("DFS average deaths: " + dfsDeath/loop);
+		System.out.println("DFS average nodes visited: " + dfsNodes/loop);
+		System.out.println("Average dfs path length: " + dfsLength/loop);
+		System.out.println("DFS final score: " + (10000.0 - (200.0*(dfsDeath/loop)) - ((dfsNodes/loop) * 3.0)- ((dfsLength/loop) * 4.0)));
+		System.out.println("-----------------------------------------------");
 	}
 }
